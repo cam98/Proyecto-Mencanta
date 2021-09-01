b333949a972104c838c96e19fa4cbf3222957b1c
Camila Bernal <camilab1998@hotmail.com>
2021-09-01 16:33:18 -0500

    Slider sirviendoooo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,52 @@
-const btns = document.querySelectorAll(".nav-btn");
-    const slides = document.querySelectorAll(".slide");
-    const contents = document.querySelectorAll(".content");
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.nav-btn');
+let currentSlide = 1;
 
-    var sliderNav = function(manual){
-      btns.forEach((btn) => {
-        btn.classList.remove("active");
-      });
+// Javascript for image slider manual navigation
+var manualNav = function(manual){
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
 
-      slides.forEach((slide) => {
-        slide.classList.remove("active");
-      });
-
-      contents.forEach((content) => {
-        content.classList.remove("active");
-      });
-
-      btns[manual].classList.add("active");
-      slides[manual].classList.add("active");
-      contents[manual].classList.add("active");
-    }
-
-    btns.forEach((btn, i) => {
-      btn.addEventListener("click", () => {
-        sliderNav(i);
-      });
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
     });
+  });
+
+  slides[manual].classList.add('active');
+  btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+ // Javascript for image slider autoplay navigation
+ var repeat = function(activeClass){
+  let active = document.getElementsByClassName('active');
+  let i = 1;
+
+  var repeater = () => {
+    setTimeout(function(){
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove('active');
+      });
+
+    slides[i].classList.add('active');
+    btns[i].classList.add('active');
+    i++;
+
+    if(slides.length == i){
+      i = 0;
+    }
+    if(i >= slides.length){
+      return;
+    }
+    repeater();
+  }, 5000);
+  }
+  repeater();
+}
+repeat();
